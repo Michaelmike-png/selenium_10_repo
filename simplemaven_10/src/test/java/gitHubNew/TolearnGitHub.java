9ad72dee0e27f5f6dd04bbf4d8080b44ea5be63a
ADMIN <ADMIN@DESKTOP-8MO0VGR>
2025-02-01 10:55:06 +0530

    New GitHub is added

---
+++ simplemaven_10/src/test/java/gitHubNew/TolearnGitHub.java
@@ -0,0 +1,19 @@
+package gitHubNew;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+
+public class TolearnGitHub {
+	
+	
+	@Test
+	public void gitnew() {
+		
+		Reporter.log("GitHub class has been crated");
+		
+	
+		
+	}
+
+}
